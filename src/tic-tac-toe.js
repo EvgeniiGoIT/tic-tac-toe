0bcf8939fd0b9616289b894396e88e25a7bae694
Evgenii <wojda007@gmail.com>
2019-10-11 15:58:14 +0300

    feat: add solution to task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,61 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.gameField = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+    this.currentPlayer = "x";
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (!this.gameField[rowIndex][columnIndex]) {
+      this.gameField[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+      this.currentPlayer = this.getCurrentPlayerSymbol() === "x" ? "o" : "x";
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    return !!this.getWinner() || this.isDraw();
+  }
 
+  getWinner() {
+    const magicSquare = [2, 7, 6, 9, 5, 1, 4, 3, 8];
+    const field = this.gameField.join().split(",");
+    let result = null;
+    for (let e of ["x", "o"]) {
+      for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field.length; j++) {
+          for (let k = 0; k < field.length; k++) {
+            if (i !== j && j !== k && k !== i) {
+              if (field[i] === e && field[j] === e && field[k] === e) {
+                if (magicSquare[i] + magicSquare[j] + magicSquare[k] === 15) {
+                  result = e;
+                }
+              }
+            }
+          }
+        }
+      }
     }
+    return result;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  noMoreTurns() {
+    return !this.gameField.reduce((acc, e) => acc.concat(e)).includes(null);
+  }
 
-    }
+  isDraw() {
+    return this.noMoreTurns() && !this.getWinner();
+  }
 
-    isFinished() {
-
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.gameField[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
